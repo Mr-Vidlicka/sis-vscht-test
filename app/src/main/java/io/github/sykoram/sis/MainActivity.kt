53a709b6f3cfc1fd850d0ff2509f433670d9ce95
Filip <krcf@vscht.cz>
2026-05-21 21:17:05 +0200

    drobne upravy

--- app/src/main/java/io/github/sykoram/sis/MainActivity.kt
+++ app/src/main/java/io/github/sykoram/sis/MainActivity.kt
@@ -111,7 +111,7 @@ class MainActivity : AppCompatActivity() {
         webView.settings.cacheMode = WebSettings.LOAD_DEFAULT
         webView.settings.javaScriptEnabled = true
         webView.visibility = View.GONE
-        webView.loadUrl("https://is.cuni.cz/studium/index.php")
+        webView.loadUrl("https://student.vscht.cz/index.php")
 
         refreshLayout = findViewById(R.id.swipeRefreshLayout)
         refreshLayout.setColorSchemeResources(R.color.primary_blue)
